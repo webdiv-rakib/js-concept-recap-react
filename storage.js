df6d1cd7d338763d2e6e9eaa22b293477e1ff49b
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-20 03:55:09 +0600

    use use localStorage.clear,setItem,remove

---
+++ storage.js
@@ -0,0 +1,16 @@
+// local storage
+// localStorage.setItem('userId', 192152868);
+const addToLocalStorage = () => {
+    const idInput = document.getElementById("storage-id");
+    const id = idInput.value;
+
+    const valueInput = document.getElementById("storage-value");
+    const value = valueInput.value;
+
+    // 
+    if(id&&value){
+        localStorage.setItem(id, value);
+    }
+    idInput.value = '';
+    valueInput.value = '';
+}
